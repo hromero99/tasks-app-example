48e8b3bdb4d8f7f8710041d81ad0f8c7aa74c8d6
Héctor Romero López <hector@hromerol.es>
2023-12-11 11:14:44 +0100

    ADD: types for thunk

--- src/features/tasks/taskThunk.ts
+++ src/features/tasks/taskThunk.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import {TaskInterface} from "../../interfaces/taskInterface";
 
-export const getRandomTasks = createAsyncThunk("task/randomTasks",async(): Promise<TaskInterface[]>=> {
+export const getRandomTasks = createAsyncThunk<TaskInterface[], void, { state: any, rejectValue: string }>("task/randomTasks",async(): Promise<TaskInterface[]>=> {
     /*const request = await fetch("https://fakerapi.it/api/v1/texts?_quantity=10&_characters=10")
     const json = await request.json()
     return json.data*/
